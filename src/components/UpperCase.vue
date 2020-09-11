4ad0e16c5a059e5ebf6829f4b8d3a9a9ddb9e609
narwalabhi <anarwal500@gmail.com>
2020-09-11 11:37:06 +0530

    initial
    .

---
+++ src/components/UpperCase.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Enter Text</h2>
+    <input v-model="message" placeholder="edit me" />
+    <p>Message is: {{ message.toUpperCase() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
